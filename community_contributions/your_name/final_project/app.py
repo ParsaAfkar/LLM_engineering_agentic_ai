e05ea48e55d27d59cd5e1798a99ea19d0970c776
Parsa <parsaafkar2006@gmail.com>
2025-12-26 11:05:34 +0330

    Update app.py

--- community_contributions/your_name/final_project/app.py
+++ community_contributions/your_name/final_project/app.py
@@ -59,3 +59,5 @@ if st.button("ساخت برنامه سفر"):
 
     result = travel_agent(prompt)
     st.text_area("برنامه پیشنهادی سفر", result, height=300)
+
+#write this text in terminal to run  :  streamlit run final_project/app.py
